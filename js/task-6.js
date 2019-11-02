6136486ebd1241dcd6bb80d7e229783ed549a38f
Oleksandr987 <51213108+Oleksandr987@users.noreply.github.com>
2019-11-02 17:09:43 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -4,12 +4,12 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', () => {
   const inputLength = input.value.length;
   console.log(inputLength);
-  if (inputLength === Number(input.dataset.length)) {
-    input.classList.add('valid');
-    input.classList.remove('invalid');
-  } else if (inputLength === 0) {
+  if (inputLength === 0) {
     input.classList.remove('invalid');
     input.classList.remove('valid');
+  } else  if (inputLength === Number(input.dataset.length)) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
   } else {
     input.classList.add('invalid');
     input.classList.remove('valid');
